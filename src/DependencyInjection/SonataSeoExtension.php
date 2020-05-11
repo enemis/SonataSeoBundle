cc60a5b1396fea9eabc1256ccf5906e49254e9d6
Andrey Stadnik <a.stadnik@propellerads.net>
2020-05-11 18:00:41 +0300

    revert loader

--- src/DependencyInjection/SonataSeoExtension.php
+++ src/DependencyInjection/SonataSeoExtension.php
@@ -33,6 +33,8 @@ class SonataSeoExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        
         $loader->load('services.xml');
 
         $this->configureSeoPage($config['page'], $container);
